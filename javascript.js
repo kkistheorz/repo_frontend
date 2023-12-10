7b1696782afbe0b833bf169b075a3ca81610e72d
kkistheorz <153403495+kkistheorz@users.noreply.github.com>
2023-12-10 06:35:20 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,23 @@
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+        const taskItem = document.createElement('li');
+        taskItem.className = 'task';
+        taskItem.innerHTML = `
+            <span>${taskText}</span>
+            <button onclick="removeTask(this)">Delete</button>
+        `;
+
+        taskList.appendChild(taskItem);
+        taskInput.value = '';
+    }
+}
+
+function removeTask(button) {
+    const taskItem = button.parentElement;
+    taskList.removeChild(taskItem);
+}
